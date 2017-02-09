5da7bea4b588409e81e617f6240398103fedc40e
Zeeshan Haider <xeieshan@gmail.com>
2017-02-09 12:59:34 +0500

    Minor

--- SampleProject/Classes/Constants/ConstantMacros_All.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ConstantMacros_All.h
-//  SampleProject
-//
-//  Created by MacPro on 15/07/2015.
-//  Copyright (c) 2015 DevBatch. All rights reserved.
-//
-//#define __ConstantMacros_all__
