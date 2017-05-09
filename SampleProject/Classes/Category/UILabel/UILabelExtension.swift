25e617fa5c6cbbac182145db8c14409c1a744bea
Zeeshan Haider <zeeshan.haider@inov8.com.pk>
2017-05-09 17:08:36 +0500

    - UILabel extensions - Info.plist update

--- SampleProject/Classes/Category/UILabel/UILabelExtension.swift
+++ SampleProject/Classes/Category/UILabel/UILabelExtension.swift
@@ -34,8 +34,16 @@ extension UILabel {
             }
         }
     }
-//    var defaultFont: UIFont? {
-//        get { return self.font }
-//        set { self.font = newValue }
-//    }
+    
+    /// Required height for a label
+    public var requiredHeight: CGFloat {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = text
+        label.attributedText = attributedText
+        label.sizeToFit()
+        return label.frame.height
+    }
 }
