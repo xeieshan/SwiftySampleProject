b1be2bb5f87ffbf84c8f27e58039895ded4b3b6a
Zeeshan Haider <xeieshan@gmail.com>
2017-11-04 04:11:20 +0500

    CameraHandler Added
    CameraHandler.shared.showActionSheet(vc: self)
    CameraHandler.shared.imagePickedBlock = { (image) in
        /* get your image here */
    }

---
+++ SampleProject/Classes/Helper/CameraHelper/CameraHandler.swift
@@ -0,0 +1,75 @@
+import Foundation
+import UIKit
+
+/*
+ CameraHandler.shared.showActionSheet(vc: self)
+ CameraHandler.shared.imagePickedBlock = { (image) in
+ /* get your image here */
+ }
+ */
+class CameraHandler: NSObject{
+    static let shared = CameraHandler()
+    
+    fileprivate var currentVC: UIViewController!
+    
+    //MARK: Internal Properties
+    var imagePickedBlock: ((UIImage) -> Void)?
+    
+    func camera()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .camera
+            currentVC.present(myPickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func photoLibrary()
+    {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .photoLibrary
+            currentVC.present(myPickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func showActionSheet(vc: UIViewController) {
+        currentVC = vc
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+            self.camera()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+            self.photoLibrary()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        vc.present(actionSheet, animated: true, completion: nil)
+    }
+    
+}
+
+
+extension CameraHandler: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        currentVC.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imagePickedBlock?(image)
+        }else{
+            print("Something went wrong")
+        }
+        currentVC.dismiss(animated: true, completion: nil)
+    }
+    
+}
